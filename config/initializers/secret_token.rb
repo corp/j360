7620710791b047ea56b4fc81da02428094a94e89
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-05-27 11:31:23 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+J360::Application.config.secret_token = '6284a50eaf0b13df29d66e37532ba7654ac611df59ebf256576174fdf949a59d1760a5f78768715f9ad72bdfd34676bff19bf0aee62486d56bb9e56d354e1ac2'
